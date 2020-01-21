85fd3d81013c4dd61bf044f6138e975a638459f6
jakubmacak <kubamacak@gmail.com>
2020-01-21 07:11:21 +0100

    Test

---
+++ 21-1-2020/index.php
@@ -0,0 +1,28 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+</head>
+
+<body>
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // ziskani hodnot z POST
+    // spojeni do jednoho retezce
+    // vypsani pomoci echo
+}
+?>
+
+<form> <!-- doplneni metody a souboru-->
+    <!-- vlozeni inputu-->
+</form>
+
+</body>
